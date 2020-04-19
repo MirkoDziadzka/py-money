791c4e31efe852388e0d339ef5b28e5115d4f4d9
Mirko Dziadzka <mirko.dziadzka@gmail.com>
2020-04-19 17:16:38 +0200

    Implement filter, close #3
    Also refactor code.

--- money/backends/MoneyMoney.py
+++ money/backends/MoneyMoney.py
@@ -14,7 +14,56 @@ def serialize(obj):
 def run_apple_script(script):
     return utils.applescript(script)
 
+def _transactions(account=None, *, age=90, start_date=None, end_date=None, **tx_filter):
+    """extract transactions from MoneyMoney which match the filter
+
+       if start_data is given, it is generated from age
+       if end_data is not give, no end_date will be assumed.
+       other argumens are treated as filter on the transactions. Currently supported:
+       - booked - the transaction is marked as booked or not yet booked
+       - checked - the checkmark is True (or False)
+       - category - the value of the category field matches the given argument
+
+       if account is given, it is expected to be an Account object
+       and only transactions from this account will be fetched.
+    """
+
+    if start_date is None:
+        start_date = datetime.date.today() - datetime.timedelta(days=age)
+    cmd = []
+    cmd += ['tell application "MoneyMoney" to export transactions']
+    if account is not None:
+        cmd += [f'from account "{account.accountNumber}"']
+    cmd += [f'from date "{start_date.strftime("%d/%m/%Y")}"']
+    if end_date is not None:
+        cmd += [f'to date "{end_date.strftime("%d/%m/%Y")}"']
+    cmd += ['as "plist"']
+    res = run_apple_script(" ".join(cmd))
+    tx_data = plistlib.loads(res)
+    for tx_data in tx_data.get("transactions", []):
+        tx = Transaction(account, tx_data)
+        if tx.pass_filter(**tx_filter):
+            yield tx
+
 class Transaction:
+    ATTRIBUTES = [
+            "accountNumber",
+            "amount",
+            "bankCode",
+            "booked",
+            "bookingDate",
+            "bookingText",
+            "category",
+            "checkmark",
+            "creditorId",
+            "currency",
+            "endToEndReference",
+            "mandateReference",
+            "name",
+            "purpose",
+            "valueDate",
+            ]
+
     def __init__(self, account, data):
         self.account = account
         self.data = self.normalize(data)
@@ -28,30 +77,28 @@ class Transaction:
                 if v <= datetime.date(year=1970, month=1, day=2):
                     continue
                 pass
-            elif k == "comment":
+            elif k in ("comment", "categoryId"):
                 continue
             res[k] = v
         return res
 
-    @property
-    def amount(self):
-        return self.data["amount"]
-
-    @property
-    def name(self):
-        return self.data["name"]
+    def __getattr__(self, name):
+        if name not in self.ATTRIBUTES:
+            raise AttributeError(name)
+        return self.data.get(name, None)
 
     @property
     def payee(self):
-        return self.data.get("accountNumber") or self.name
+        return self.accountNumber or self.name
 
-    @property
-    def booked(self):
-        return self.data["booked"]
-
-    @property
-    def checkmark(self):
-        return self.data["checkmark"]
+    def pass_filter(self, *, booked=None, checked=None, category=None):
+        if booked is not None and self.booked != booked:
+            return False
+        if checked is not None and self.checkmark != checked:
+            return False
+        if category is not None and self.category != category:
+            return False
+        return True
 
     def set_checkmark(self, *, value=True):
         assert isinstance(value, bool)
@@ -78,13 +125,9 @@ class Account:
     def accountNumber(self):
         return self.data["accountNumber"]
 
-    def transactions(self):
-        start = (datetime.datetime.now() - datetime.timedelta(days=90))
-        cmd = f'tell application "MoneyMoney" to export transactions from account "{self.data["accountNumber"]}" from date "{start.strftime("%d/%m/%Y")}" as "plist"'
-        res = run_apple_script(cmd)
-        tx_data = plistlib.loads(res)
-        for tx in tx_data.get("transactions", []):
-            yield Transaction(self, tx)
+
+    def transactions(self, *args, **kwargs):
+        return _transactions(account=self, *args, **kwargs)
 
     def __repr__(self):
         return json.dumps(self.data, separators=(',', ':'), default=serialize)
@@ -100,6 +143,9 @@ class MoneyMoney:
         for account in self.data:
             if account.get("accountNumber"):
                 yield(Account(account))
+
+    def transactions(self, *args, **kwargs):
+        return _transactions(account=None, *args, **kwargs)
 
 
 
--- test.py
+++ test.py
@@ -22,27 +22,44 @@ FIELDS = [
     "endToEndReference",
     "creditorId",
     "mandateReference",
+    "category",
     "bookingDate",
     "valueDate",
     "checkmark"
 ]
 
+#
+# Transactions of the last 7 days
+#
+
+print("Show all new, booked and unchecked transactions")
+for account in instance.accounts():
+    for tx in account.transactions(age=30, booked=True, checked=False):
+        assert tx.booked == True and tx.checkmark == False
+        print(f'Found new transaction: {account.name} {tx.bookingDate} {tx.amount:10.2f} {tx.currency} {tx.name}')
+        tx.set_checkmark(value=True)  # mark this as seen
+
+
+category="Spende"
+
+print(f"Show all transactions which are marked as category '{category}'")
+for tx in instance.transactions(age=30, booked=True, category=category):
+    assert tx.booked == True and tx.category == category
+    print(f'Found new transaction: {tx.bookingDate} {tx.amount:10.2f} {tx.currency} {tx.name}')
+
+print(f"Convert all transactions to csv '{FILENAME}'")
 
 with open(FILENAME, "w") as fd:
-    # writer = csv.DictWriter(fd, FIELDS, restval='', extrasaction='ignore')
     writer = csv.DictWriter(fd, FIELDS, restval='', extrasaction='raise')
     writer.writeheader()
 
-
     for account in instance.accounts():
-        print(f"Checking Account: {account.name}")
-        for tx in account.transactions():
-            if not tx.booked:
-                print(f"Ignore unbooked transaction: {tx}")
-                continue
+        print(f"Writing data for account {account.name} ...", end='', flush=True)
+        count = 0
+        for tx in account.transactions(start_date = datetime.date(year=2000, month=1, day=1), booked=True):
+            assert tx.booked == True
             row = dict(tx.data, account=tx.account.name)
             writer.writerow(row)
-            if not tx.checkmark:
-                print(f"New transaction: {tx}")
-                tx.set_checkmark(value=True)
+            count += 1
+        print(f"wrote {count} entries")
 
