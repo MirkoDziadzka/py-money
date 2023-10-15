d433e6469c92e6483d8b1e1f1b91a81389736d7c
Mirko Dziadzka <mirko.dziadzk@gmail.com>
2023-10-15 21:53:28 +0200

    cleanup

--- money/utils.py
+++ money/utils.py
@@ -1,7 +1,7 @@
 """
 Some helper functions.
 """
-import subprocess
+from subprocess import Popen, PIPE
 
 
 class AppleScriptException(Exception):
@@ -13,13 +13,10 @@ def applescript(cmd: str) -> bytes:
 
     Return the output of the command or raise an exception on failure.
     """
-    proc = subprocess.Popen(
-        ["osascript", "-"],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    out, err = proc.communicate(input=cmd.encode("utf-8"), timeout=60)
-    if proc.returncode != 0:
-        raise AppleScriptException(f'{err.decode("utf-8")}')
-    return out
+    command = ["osascript", "-"]
+
+    with Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
+        out, err = proc.communicate(input=cmd.encode("utf-8"), timeout=60)
+        if proc.returncode != 0:
+            raise AppleScriptException(f'{err.decode("utf-8")}')
+        return out
