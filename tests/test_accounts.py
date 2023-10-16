245f9b3940b312004e4078d1d1d653e23a0ce243
Mirko Dziadzka <mirko.dziadzk@gmail.com>
2023-10-16 23:17:05 +0200

    -

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -1,51 +1,9 @@
 """ unit tests
 """
 
-from datetime import date
-import os.path
-from typing import Optional
-
 import pytest
-import yaml
 
 from money import MoneyMoney
-from money.backends.MoneyMoney import BackendInterface
-
-TESTDIR = os.path.dirname(__file__)
-
-
-class MockedBackend(BackendInterface):
-    def __init__(self, data):
-        self.data = data
-
-    def get_accounts(self):
-        return self.data["accounts"]
-
-    def get_positions(self, account: str):
-        return self.data["positions"][account]
-
-    def get_transactions(self, account: str, start_date: date, end_date: Optional[date]):
-        return self.data["transactions"][account]
-
-    def set_transaction_field(self, txid: str, name: str, value: str):
-        print(txid, name, value)
-        for txlist in self.data["transactions"].values():
-            for tx in txlist:
-                print(tx)
-                if tx["id"] == txid:
-                    if name == "checkmark":
-                        tx["checkmark"] = value == "on"
-                    else:
-                        raise NotImplementedError
-
-
-# pylint: disable=redefined-outer-name
-@pytest.fixture
-def instance() -> MoneyMoney:
-    with open(os.path.join(TESTDIR, "backend_config.yml"), "rb") as fd:
-        data = yaml.safe_load(fd.read())
-        backend = MockedBackend(data)
-        return MoneyMoney(backend=backend)
 
 def test_accounts_are_present(instance: MoneyMoney):
     accounts = list(instance.accounts())
